868adf67b34f03ae1d49dadaf5acef7debe8be8c
Dmitry-Co <87475279+Dmitry-Co@users.noreply.github.com>
2023-08-11 00:19:02 +0500

    Create triangle_area

---
+++ Lesson_1/triangle_area.rb
@@ -0,0 +1,12 @@
+# Triangle area calculation.
+
+puts "Hello! 
+We are going to calculate the triangle area."
+
+puts "Please input base value."
+a = Integer(gets.chomp)
+
+puts "Please input height value."
+h = Integer(gets.chomp)
+
+puts "The triangle area is #{0.5*a*h}"
