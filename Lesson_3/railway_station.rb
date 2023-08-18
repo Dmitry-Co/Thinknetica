05265045b4fafdef7c94722dccca515f7fcc02a1
Dmitry-Co <87475279+Dmitry-Co@users.noreply.github.com>
2023-08-19 00:33:09 +0500

    3rd updating railway_station.
    1) Adding attr_reader :stations to the Route class.
    2) Deleting method stations_list in the Route class.

--- Lesson_3/railway_station.rb
+++ Lesson_3/railway_station.rb
@@ -24,10 +24,10 @@ class Station
   end
 end
 
-# route.stations
-
 # 2
 class Route
+  attr_reader :stations
+
   def initialize(start_station, end_station)
     @stations = [start_station, end_station]
   end
@@ -39,10 +39,6 @@ class Route
 
   def delete_station(station)
     @stations.delete(station)
-  end
-
-  def stations_list
-    [start_station, mid_stations, end_station].flatten!.compact
   end
 end
 
