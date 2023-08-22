620aa3559ba8989d023071c6f219b2d6c9485dc2
Dmitry-Co <87475279+Dmitry-Co@users.noreply.github.com>
2023-08-22 19:33:17 +0500

    Updating main.rb
    Adding:
    - class Main
    - show_menu method
    - start method
    - gets_choice method
    - create_station method
    - create_train method

--- Lesson_4/main.rb
+++ Lesson_4/main.rb
@@ -5,6 +5,104 @@ require_relative 'lib/passenger_train'
 require_relative 'lib/passenger_vagon'
 require_relative 'lib/cargo_train'
 
+class Main
+  ACTIONS = [
+    { id: '1', title: 'Создать станцию', action: :create_station },
+    { id: '2', title: 'Создать поезд', action: :create_train },
+    { id: '3', title: 'Создать маршрут', action: :create_route },
+    { id: '4', title: 'Управлять станциями(добавлять и удалять)', action: :station_action },
+    { id: '5', title: 'Назначить маршрут поезду', action: :add_train_route },
+    { id: '6', title: 'Добавить или удалить вагон у поезда', action: :wagon_actions },
+    { id: '7', title: 'Перемещать поезд по маршруту(вперед и назад)', action: :move_train },
+    { id: '8', title: 'Показать список станций', action: :check_stations },
+    { id: '9', title: 'Показать список поездов на станции', action: :check_trains },
+     {id: '0', title: 'Выход', action: :exit }
+  ]
+
+  def initialize
+    @trains = []
+    @stations = []
+    @routes = []
+  end
+
+  def start
+    loop do
+      show_menu
+      choice = gets_choice
+      break if call_action(choice) == "0"
+    end
+  end
+
+  private
+
+  def show_menu
+    puts "Нажмите 1 для создания станции"
+    puts "Нажмите 2 для создания поезда"
+    puts "Нажмите 3 для создания маршрута"
+    puts "Нажмите 4 для удаления или добавления станции" 
+    puts "Нажмите 5 для назначения маршрута поезду" 
+    puts "Нажмите 6 для добавления или удаления вагона у поезда" 
+    puts "Нажмите 7 для перемещения поезда по маршруту" 
+    puts "Нажмите 8 для показа списка станций" 
+    puts "Нажмите 9 для показа списка поездов на станции" 
+    puts "Нажмите 0, для выхода"
+  end
+
+  def gets_choice
+    input = gets.chomp
+    hash_of_action = ACTIONS.find { |action| action[:id] == input }
+    if hash_of_action
+      method_name = hash_of_action[:action] 
+    else
+      puts "Данные введены некоректно!"
+    end
+  end
+  
+  def call_action(choice)
+    send(choice) if choice
+  end
+  
+  def create_station
+    puts "Введите название для станции: "
+    begin
+      title = gets.chomp
+      @stations << Station.new(title)
+      puts "Станция создана!"
+    rescue => error
+      p error
+      puts "Данные введены некоректно, попробуйте еще раз!"
+      retry
+    end
+  end
+
+  def create_train
+    puts "Какой тип поезда вы хотите создать:
+    Нажмите 1 для создания грузового поезда,
+    Нажмите 2 для создания пассажирского поезда"
+    type = gets.chomp
+    loop do 
+      break if type == "1" || type == "2"
+      puts "Пожалуйста, выберайте только 1 или 2"
+      type = gets.chomp
+    end
+    puts "Какой номер у поезда?"
+
+    begin
+      number = gets.chomp
+      case type 
+      when "1"
+        puts "Поезд #{@trains << CargoTrain.new(number)} создан!"
+      when "2"
+        puts "Поезд #{@trains << PassengerTrain.new(number)} создан!"
+      end
+    rescue => error
+      p error
+      puts "Данные введены некоректно, попробуйте еще раз!"
+      retry
+    end
+  end
+
+Main.new.start
 
 # tests
 station_ekb = Station.new('Ekaterinburg')
